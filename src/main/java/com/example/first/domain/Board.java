5bb01ef15596497e4b059a2881948840c7f27419
ajy4304 <ajy4304@naver.com>
2023-08-28 02:37:50 +0900

    Create Member, Board,  MemberRepository and BoardRepository

--- src/main/java/com/example/first/domain/Board.java
+++ src/main/java/com/example/first/domain/Board.java
@@ -1,19 +1,23 @@
 package com.example.first.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "board")
+@Entity // 엔티티 지정
+@Table(name = "board") // 엔티티와 매핑할 테이블 지정
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 파라미터가 없는 기본 생성자 생성. 외부에서 무분별하게 해당 엔티티 객체 생성을 방지하기 위해 PROTECTED
+// application.properties에 spring.jpa.hibernate.ddl-auto=update 을 해주면
+// 엔티티 클래스에서 정의한 테이블이 데이터베이스에 존재하지 않을 때 해당 테이블을 자동생성해줌.
 public class Board {
 
-    @Id
+    @Id // 엔티티의 PK값 지정
     @Column(name = "board_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 식별자(PK) 자동 생성. 오토 인크리먼트
     private Long boardId;
 
     @Column(name = "title")
@@ -25,4 +29,11 @@ public class Board {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Builder
+    public Board(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
 }
--- src/main/java/com/example/first/domain/Member.java
+++ src/main/java/com/example/first/domain/Member.java
@@ -1,21 +1,22 @@
 package com.example.first.domain;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "member")
+@Entity // 엔티티 지정
+@Table(name = "member") // 엔티티와 매핑할 테이블 지정
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 생성
 public class Member {
 
 
-    @Id
+    @Id // 엔티티 PK값 지정
     @Column(name ="member_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 자동 생성, 오토 인크리먼트
     private Long memberId;
 
     @Column(name = "email")
