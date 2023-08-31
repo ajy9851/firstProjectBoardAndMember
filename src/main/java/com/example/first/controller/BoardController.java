83e9d1747af25a1dfe5e1881927173fa053f2fef
ajy4304 <ajy4304@naver.com>
2023-08-31 16:44:13 +0900

    Add: readBoard to BoardController

--- src/main/java/com/example/first/controller/BoardController.java
+++ src/main/java/com/example/first/controller/BoardController.java
@@ -2,13 +2,12 @@ package com.example.first.controller;
 
 import com.example.first.dto.BoardCreateRequestDTO;
 import com.example.first.dto.BoardCreateResponseDTO;
+import com.example.first.dto.BoardReadResponseDTO;
 import com.example.first.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,5 +19,11 @@ public class BoardController {
     public ResponseEntity createBoard(@RequestBody BoardCreateRequestDTO boardCreateRequestDTO) {
         BoardCreateResponseDTO boardCreateResponseDTO = boardService.createBoard(boardCreateRequestDTO);
         return ResponseEntity.ok(boardCreateResponseDTO);
+    }
+
+    @GetMapping("/read/{boardId}")
+    public ResponseEntity readBoard(@PathVariable Long boardId) {
+        BoardReadResponseDTO boardReadResponseDTO = boardService.readBoard(boardId);
+        return ResponseEntity.ok(boardReadResponseDTO);
     }
 }
