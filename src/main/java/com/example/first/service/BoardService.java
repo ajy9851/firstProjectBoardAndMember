ee1f5b82f44d334611bad3f44bb41785beba6a6f
ajy4304 <ajy4304@naver.com>
2023-08-31 16:12:05 +0900

    Add: BoardService, createBoard in BoardService

--- src/main/java/com/example/first/service/BoardService.java
+++ src/main/java/com/example/first/service/BoardService.java
@@ -1,5 +1,28 @@
 package com.example.first.service;
+import com.example.first.domain.Board;
 import com.example.first.dto.*;
+import com.example.first.repository.BoardRepository;
+import com.example.first.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+    private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public BoardCreateResponseDTO createBoard(BoardCreateRequestDTO boardCreateRequestDTO) {
+        Board board = boardCreateRequestDTO.boardDtoToEntity(boardCreateRequestDTO);
+        board.boardSetMember(memberRepository.findByEmail(boardCreateRequestDTO.getEmail()));
+        boardRepository.save(board);
+        return BoardCreateResponseDTO.builder()
+                .email(boardCreateRequestDTO.getEmail())
+                .title(boardCreateRequestDTO.getTitle())
+                .content(boardCreateRequestDTO.getContent())
+                .boardId(board.getBoardId())
+                .build();
+    }
 }
