911a2fb20973bbd0af06d27c956a573fb80dfd1a
ajy4304 <ajy4304@naver.com>
2023-08-31 12:18:48 +0900

    Add: MemberUpdatePwdResponseDTO

--- src/main/java/com/example/first/dto/MemberUpdatePwdResponseDTO.java
+++ src/main/java/com/example/first/dto/MemberUpdatePwdResponseDTO.java
@@ -1,2 +1,17 @@
-package com.example.first.dto;public class MemberUpdatepwdResponseDTO {
+package com.example.first.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberUpdatePwdResponseDTO {
+    private String email;
+
+    @Builder
+    public MemberUpdatePwdResponseDTO(String email){
+        this.email = email;
+    }
+
 }
